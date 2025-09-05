2545b9132217f4954d09a18059d841700940f369
VK4041 <151887513+VK4041@users.noreply.github.com>
2025-09-05 12:28:44 +1000

    Finalise layout and add button listeners
    All buttons have been linked with click event listener
    logging onto console for now

--- script.js
+++ script.js
@@ -1,16 +1,120 @@
-const body = document.querySelector('body')
-const footer = document.querySelector('.footer')
-const container = document.querySelector('.container')
-const mainBody = document.querySelector('.mainBody')
-const title = document.querySelector('.title')
-const rows = Array.from(document.querySelectorAll('.row'))
+const screen = document.querySelector('.screen')
+function createNumpad(numpadRow) {
+    const numPad = document.createElement('div')
+    const numRows = Array.from({ length: 4 }, () => document.createElement('div'));
 
-//container.classList.add('')
-title.classList.add('text-center')
-mainBody.classList.add('flex-col')
-footer.classList.add('text-center')
-body.classList.add('flex-col')
-rows.map(row => row.classList.add('flex-row'))
+    numPad.classList.add('flex-col', 'numPad')
+    numRows.forEach(each => each.classList.add('flex-row'))
+    let num = 9
 
-const numPad = createNumpad(rows[1])
-const operators = createOperators(rows[1])
+    numRows.slice(0, -1).forEach(row => {
+        while (row.hasChildNodes()) row.removeChild(row.firstElementChild)
+
+        let numBtn = getButton()
+        numBtn.textContent = num--
+        row.appendChild(numBtn)
+
+        while (num % 3) {
+            let newNumBtn = getButton()
+            newNumBtn.textContent = num--
+            row.insertBefore(newNumBtn, row.firstChild)
+        }
+        numPad.appendChild(row);
+    })
+    let zeroBtn = getButton()
+    let zeroDiv = numRows[3]
+
+    zeroBtn.textContent = num
+    zeroDiv.classList.add('left')
+
+    zeroDiv.appendChild(zeroBtn)
+    numPad.appendChild(zeroDiv)
+    numpadRow.appendChild(numPad)
+    numpadRow.classList.add('flex-row')
+    return numPad
+}
+function createOperators(numpadRow) {
+    const operators = document.createElement('div')
+    const opRows = Array.from({ length: 3 }, () => document.createElement('div'))
+
+    let opText = ['+', '-', 'x', '÷', '=']
+    let opBtns = Array.from({ length: 5 }, () => getButton())
+    opBtns.forEach((btn, index) => {
+        btn.textContent = opText[index]
+        //btn.classList.add('btn')
+    })
+    let i = 0;
+    let j = -1;
+    while (i < 5) {
+        if (i % 2 == 0) {
+            opRows[++j].appendChild(opBtns[i++])
+        }
+        else {
+            opRows[j].appendChild(opBtns[i++])
+        }
+    }
+    opRows.forEach(row => {
+        operators.appendChild(row)
+        row.classList.add('flex-row')
+    })
+    opRows[2].classList.add('right')
+    numpadRow.appendChild(operators)
+    operators.classList.add('flex-col', 'operators')
+    return operators
+}
+function getButton() {
+    return document.createElement('button')
+}
+function displayScreen() {
+    const screenText = screen.firstElementChild
+    screen.classList.add('flex-col')
+    screenText.classList.add('right')
+}
+function attachListeners(btnRow1, numPad, operators) {
+    btnRow1.childNodes.forEach(btn => {
+        btn.addEventListener('click', (e) => console.log(e.target.textContent))
+    })
+
+    numPad.childNodes.forEach(row => {
+        row.childNodes.forEach(numBtn => {
+            numBtn.addEventListener('click', (e) => console.log('Num: ' + e.target.textContent))
+        })
+    })
+
+    operators.childNodes.forEach((row) => {
+        row.childNodes.forEach(opBtn => {
+            opBtn.addEventListener('click', (e) => console.log(e.target.textContent))
+        })
+    })
+}
+const add = (num1, num2) => num1 + num2
+const subtract = (num1, num2) => num1 - num2
+const multiply = (num1, num2) => num1 * num2
+const divide = (num1, num2) => {
+    if (num2 === 0) {
+        alert('Error, division by zero')
+        return 0
+    }
+    return num1 / num2
+}
+function main() {
+    const body = document.querySelector('body')
+    const footer = document.querySelector('.footer')
+    const container = document.querySelector('.container')
+    const mainBody = document.querySelector('.mainBody')
+    const title = document.querySelector('.title')
+    const row1 = document.querySelector('.row1')
+    const row2 = document.querySelector('.row2')
+
+    //container.classList.add('')
+    row1.classList.add('flex-row')
+    title.classList.add('text-center')
+    mainBody.classList.add('flex-col')
+    footer.classList.add('text-center')
+    body.classList.add('flex-col')
+    const numPad = createNumpad(row2)
+    const operators = createOperators(row2)
+    attachListeners(row1, numPad, operators)
+    displayScreen()
+}
+main()
